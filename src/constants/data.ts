4cb1d2001bded801d7915a5e6ba218a411ee1a1c
sjoleee <cent7425@gmail.com>
2024-12-22 23:09:03 +0900

    article: 2024 회고

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,17 +1,18 @@
 export const USER_INFORMATIONS = {
-  "headerTitle": "sjoleee",
-  "userName": "이상조",
-  "mascot": "🐶",
-  "resume": "https://www.sjoleee.info",
-  "github": "https://github.com/sjoleee",
-  "domain": "https://blog.sjoleee.info",
-  "meta": {
-    "title": "rent for developers | sjoleee blog",
-    "description": "개발 지식 빌려드립니다🐶"
-  }
+  headerTitle: "sjoleee",
+  userName: "이상조",
+  mascot: "🐶",
+  resume: "https://www.sjoleee.info",
+  github: "https://github.com/sjoleee",
+  domain: "https://blog.sjoleee.info",
+  meta: {
+    title: "rent for developers | sjoleee blog",
+    description: "개발 지식 빌려드립니다🐶",
+  },
 };
 
 export const CATEGORY_ICONS: Record<string, string> = {
+  Retrospect: "⏳",
   Develop: "🧑🏻‍💻",
   All: "🏠",
 };
